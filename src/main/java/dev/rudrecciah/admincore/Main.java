ad3cba53cabbc049b17e49d2aa3b693457d32c4e
RudRecciah <me@rudrecciah.dev>
2021-03-05 01:37:53 -0500

    added staffchat

--- src/main/java/dev/rudrecciah/admincore/Main.java
+++ src/main/java/dev/rudrecciah/admincore/Main.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -29,14 +30,7 @@ public final class Main extends JavaPlugin implements CommandExecutor, Listener 
         getLogger().info("Admin Core Enabled");
     }
 
-    /*
-    * How staffchat will work:
-    * Player uses /staffchat to enter staffchat and see messages and talk to staff
-    * Every time a message is sent from a player in staffchat, it gets sent to only other staff in staffchat
-    * Every time a message is sent in staffchat, staff who are not in staffchat will get a notification
-    * Config Options:
-    * Pings for staff (if staff hear a notification as well as see one in chat): false
-    * */
+    //TODO: make staffchat pings toggleable for each staff member rather than the whole plugin
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -44,34 +38,32 @@ public final class Main extends JavaPlugin implements CommandExecutor, Listener 
             Player p = (Player) sender;
             if(listeningToStaffChat.contains(p)) {
                 listeningToStaffChat.remove(p);
-                p.sendMessage(ChatColor.YELLOW + "Chat mode:" + ChatColor.BOLD + "Global");
+                p.sendMessage(ChatColor.YELLOW + "Chat mode: " + ChatColor.BOLD + "Global");
             }else{
                 listeningToStaffChat.add(p);
-                p.sendMessage(ChatColor.YELLOW + "Chat mode:" + ChatColor.BOLD + "Staff");
+                p.sendMessage(ChatColor.YELLOW + "Chat mode: " + ChatColor.BOLD + "Staff");
             }
         }
         return true;
     }
 
     @EventHandler
-    public void onMessageSend(AsyncPlayerChatEvent e, Player p, String message, Set<Player> playerSet) {
+    public void onMessageSend(AsyncPlayerChatEvent e) {
+        Player p = e.getPlayer();
+        String message = e.getMessage();
+        Set<Player> playerSet = e.getRecipients();
         if(listeningToStaffChat.contains(p)) {
-            for(Player player : listeningToStaffChat) {
-                player.sendMessage(message);
-            }
+            playerSet.clear();
+            playerSet.addAll(listeningToStaffChat);
             List<Player> players = (List) getServer().getOnlinePlayers();
             for(Player player : players) {
-                if(player.hasPermission("admincore.staff")) {
-                    player.sendMessage(ChatColor.YELLOW + "A staff member has sent a message in the staff chat room. Use /staffchat to join." + "(" + message + ")");
+                if(player.hasPermission("admincore.staff") && !listeningToStaffChat.contains(player)) {
+                    player.sendMessage(ChatColor.BLUE + "" + ChatColor.BOLD + "[STAFF CHANNEL] " + ChatColor.YELLOW + p.getName() + ": " + message);
                     FileConfiguration config = plugin.getConfig();
                     if(config.getBoolean("staffchatpings")) {
                         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 1f);
                     }
                 }
-            }
-        }else{
-            for(Player player : playerSet) {
-                player.sendMessage(message);
             }
         }
     }
