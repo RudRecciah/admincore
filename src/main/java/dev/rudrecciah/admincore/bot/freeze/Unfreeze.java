7271e8c672c7c2dd25a20a2e0571409ea5c84a44
RudRecciah <me@rudrecciah.dev>
2021-03-29 22:47:17 -0400

    added icons

---
+++ src/main/java/dev/rudrecciah/admincore/bot/freeze/Unfreeze.java
@@ -0,0 +1,9 @@
+package dev.rudrecciah.admincore.bot.freeze;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class Unfreeze {
+    public static void handle(String[] contents, MessageChannel channel) {
+
+    }
+}
---
+++ src/main/java/dev/rudrecciah/admincore/bot/help/Help.java
@@ -0,0 +1,11 @@
+package dev.rudrecciah.admincore.bot.help;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class Help {
+    public static void handle(String[] contents, MessageChannel channel) {
+        EmbedBuilder embed = new EmbedBuilder();
+
+    }
+}
