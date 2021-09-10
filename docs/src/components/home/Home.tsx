ea25803ec1a157a9a4fbffce129cde651b67f0de
RudRecciah <me@rudrecciah.dev>
2021-09-10 18:23:39 -0400

    more homepage things

--- docs/src/components/home/Home.tsx
+++ docs/src/components/home/Home.tsx
@@ -6,6 +6,7 @@ import splash from "../../static/splash.png";
 import PathController from "../../utils/PathController";
 import { Link } from "react-router-dom";
 import inspect from "../../static/inspect.png";
+import punish from "../../static/punish.png";
 
 interface Props {
 
@@ -44,6 +45,19 @@ const Home: FC<Props> = (): ReactElement => {
               <div className={css.inspectCopy}>
                 <h1 className={css.landingHeader}>Never question a player's identity</h1>
                 <p className={css.landingText}>With Admincore, you can inspect your entire playerbase for information like their past punishments, alternate accounts, and even malicious activity across the internet with <a href={"https://www.ipqualityscore.com/"} className={"text-decoration-none text-link"}>IPQS</a> integration.</p>
+              </div>
+            </Col>
+          </Row>
+          <Row className={"flex-column flex-lg-row"}>
+            <Col xs={12} lg={6} className={"text-center text-lg-start"}>
+              <div className={css.punishCopy}>
+                <h1 className={css.landingHeader}>Keep your playerbase in check</h1>
+                <p className={css.landingText}>With easy-to-use player reporting and punishment handling, you can maintain a healthy playerbase. Account ban, IP ban, and mute players using Admincore's UI.</p>
+              </div>
+            </Col>
+            <Col xs={12} lg={6} className={"text-center text-lg-end"}>
+              <div className={css.punishImageContainer}>
+                <img src={punish} alt={"Punish players easily."} className={`img-fluid ${css.punishImage}`}/>
               </div>
             </Col>
           </Row>
